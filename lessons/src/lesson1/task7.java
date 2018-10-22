2f2a8d8d8e5a22cfd06a35f3501812dce77d926f
igortrofimov <iigortrofimov@yandex.ru>
2018-10-22 15:57:46 +0300

    task7

--- lessons/src/lesson1/task7.java
+++ lessons/src/lesson1/task7.java
@@ -2,12 +2,12 @@ package lesson1;
 
 public class task7 {
     public static void main(String[] args) {
-        int num = 645, num1 = num / 100, num2 = num / 10 % 10, num3 = num % 10;
+        int num = 787, num1 = num / 100, num2 = num / 10 % 10, num3 = num % 10;
         if (num3 >= num1 && num3 >= num2)
         {
             System.out.println(num3);
         }
-        else if (num2 >= num1 && num3 >= num2)
+        else if (num2 >= num1 && num2 >= num3)
         {
             System.out.println(num2);
         }
