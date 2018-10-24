9df00d3461919c86350a3e177fa1be5d1a5c5bbb
igortrofimov <iigortrofimov@yandex.ru>
2018-10-24 20:14:24 +0300

    //

--- lessons/src/lesson3/task4.java
+++ lessons/src/lesson3/task4.java
@@ -1,21 +1,38 @@
 package lesson3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class task4 {
     public static void main(String[] args) {
-        System.out.println("Введите положительное четкое число");
+        int sumModLeft = 0;
+        int sumModRight = 0;
+        int num;
+        System.out.println("Введите положительное четное число");
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        Random rand = new Random();
-        if (num > 0 && num % 2 ==0){
-            int[] arr = new int[num];
-            for (int i = 0; i < arr.length; i++){
-                int n = rand.nextInt(10);
-                arr[i] = n;
-            }
-        }
+        if (in.hasNextInt()) {
+            do {num = in.nextInt();
+                if (num % 2 != 0 || num < 1) {
+                    System.out.println("Введите правильное число");
+                }
+            } while (num % 2 != 0 || num < 1);
 
+                int[] arr = new int[num];
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = (int) (Math.random() * (10 + 1) - 5); // //( Math.random() * (b-a + 1) ) + a
+                    System.out.println(arr[i] + " ");
+                    if (i < arr.length / 2 ) {
+                        sumModLeft = Math.abs(arr[i]) + sumModLeft;
+                    } else
+                        sumModRight = Math.abs(arr[i]) + sumModRight;
+                }
+                if (sumModLeft > sumModRight) {
+                    System.out.println("Сумма модулей левой половины массива больше = "+ sumModLeft);
+                } else if (sumModLeft == sumModRight) {
+                    System.out.println("модули равны");
+                } else System.out.println("Сумма модулей правой половины массива больше = " + sumModRight);
+            }else System.out.println("Введено не число");
     }
 }
+
--- lessons/src/lesson3/task_string1.java
+++ lessons/src/lesson3/task_string1.java
@@ -1,9 +1,19 @@
 package lesson3;
 
+import java.util.Scanner;
+
 public class task_string1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
-            System.out.println(-5 + (int)(Math.random() * (5 -(-5))) + " ");
+        Scanner in = new Scanner(System.in);
+        String str1 = in.nextLine();
+        String str2 = in.nextLine();
+        String str3 = in.nextLine();
+        System.out.println(str1.replaceAll(str2, str3));
+
+//replaceall
+        //StringBuffer
+        // delet char
+// 2 цикла for + if
         }
     }
-}
+
